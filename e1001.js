81cdd9afdf6cb92d4f1f97f59f05b9a65c81f9de
Eduardo Staudt de Castilhos <edu.staudt00@gmail.com>
2023-07-26 20:41:11 -0300

    1001

--- e1001.js
+++ e1001.js
@@ -1,8 +1,8 @@
-var input = require('fs').readFileSync('stdin', 'utf8')
-var line = input.split('\n')
+var input = require('fs').readFileSync('stdin', 'utf8');
+var lines = input.split('\n');
 
-var a = parseInt(line.shift())
-var b = parseInt(line.shift())
-var x = a + b
+var a = parseInt(lines.shift());
+var b = parseInt(lines.shift());
+var x = a + b;
    
-console.log('X = ' + x)
+console.log('X = ' + x);
